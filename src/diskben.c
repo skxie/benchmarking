d9f2ed1f804f8386c7232fa7ddf4ff5305fb02af
shukun <shuk.xie@gmail.com>
2013-02-05 10:07:47 -0600

    modified disk

--- src/diskben.c
+++ src/diskben.c
@@ -11,12 +11,15 @@ void *ranRead(void *n) {
 	int  *size  = (int *)n;
 	FILE *pFile = fopen(FILE_NAME, "r");
 	unsigned long m = 1024ul * 1024ul * 1024ul * 2ul;
-	srand((int)time(0));
-	unsigned long pos = rand() % (m - *size);
-	int i   = 0;
+	unsigned long pos = 0;
+  	int i   = 0;
 	char * buffer = (char *)malloc((*size)*sizeof(char));
-	fseek(pFile, pos, SEEK_SET);
-	fread(buffer, *size, 1, pFile);
+	for (i = 0; i < 10; i++) {
+		srand((int)time(0));
+	 	pos = rand() % (m - *size);
+		fseek(pFile, pos, SEEK_SET);
+		fread(buffer, *size, 1, pFile);
+	}
 	fclose(pFile);
 }
 
@@ -25,39 +28,43 @@ void *ranWrite(void *n) {
 	FILE *pFile = fopen(FILE_NAME, "r+");
 	fseek(pFile, 0, SEEK_SET);
 	unsigned long m =  1024ul * 1024ul * 1024ul * 2ul;
-	srand((int)time(0));
-	unsigned long pos = rand() % (m - *size);
+	unsigned long pos = 0;
 	int i   = 0;
 	char * buffer = (char *)malloc((*size)*sizeof(char));
-	fseek(pFile, pos, SEEK_SET);
-	fwrite(buffer, *size, 1, pFile);
+	for (i = 0; i < 10; i++) {
+		srand((int)time(0));
+		pos = rand() % (m - *size);
+		fseek(pFile, pos, SEEK_SET);
+		fwrite(buffer, *size, 1, pFile);
+	}
 	fclose(pFile);
 }
 
 void *seqRead(void *n) {
 	int  *size  = (int *)n;
 	FILE *pFile = fopen(FILE_NAME, "r");
-	int i   = 0;
-	for (i = 0; i < *size; i++) {
-		fgetc(pFile);
+	char *buffer = (char *)malloc((*size)*sizeof(char));
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		fread(buffer, *size, 1, pFile);
 	}
 	fclose(pFile);
 }
 
 void *seqWrite(void *n) {
 	int  *size  = (int *)n;
+	char *buffer = (char *)malloc((*size)*sizeof(char));
 	FILE *pFile = fopen(FILE_NAME, "r+");
 	fseek(pFile, 0, SEEK_SET);
-	int i	= 0;
-	char b  = 'b';
-	for (i = 0; i < *size; i++) {
-		fputc(b, pFile);
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		fwrite(buffer, *size, 1, pFile);
 	}
 	fclose(pFile);
 }
 
 int main() {
-
+	/*
 	FILE *pFile = fopen(FILE_NAME, "w");
 	unsigned  long k = 0;
 	char a = 'a';
@@ -66,9 +73,10 @@ int main() {
 		fputc(a, pFile);
 	}
 	fclose(pFile);
+	*/
+
 	srand((int)time(0));
 	int i = 0;
-	
 	for (i = 1; i <=2; i++) {
 		int j = 0;
 		for (j = 0; j < 4; j++) {
@@ -83,7 +91,7 @@ int main() {
 			for (k = 0; k < i; k++)
 				pthread_join(pthreads[k], NULL);
 			gettimeofday(&end_t, NULL);
-			double exec_t = 1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0;
+			double exec_t = (1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0)/10.0;
 			double throughput = (n / (1024.0 * 1024.0)) / (exec_t / 1000.0);
 			printf("With %d threads, sequential write %10d bytes, the latency is %10f ms and the throughput     is %10f MB/sec\n", i, n, exec_t, throughput);
 
@@ -93,7 +101,7 @@ int main() {
 			for (k = 0; k < i; k++)
 				pthread_join(pthreads[k], NULL);
 			gettimeofday(&end_t, NULL);
-			exec_t = 1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0;
+			exec_t = (1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0)/10.0;
 			throughput = (n / (1024.0 * 1024.0)) / (exec_t / 1000.0);
 			printf("With %d threads, sequential  read %10d bytes, the latency is %10f ms and the throughput     is %10f MB/sec\n", i, n, exec_t, throughput);
 
@@ -103,7 +111,7 @@ int main() {
 			for (k = 0; k < i; k++)
 				pthread_join(pthreads[k], NULL);
 			gettimeofday(&end_t, NULL);
-			exec_t = 1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0;
+			exec_t = (1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0)/10.0;
 			throughput = (n / (1024.0 * 1024.0)) / (exec_t / 1000.0);
 			printf("With %d threads, randomly   write %10d bytes, the latency is %10f ms and the throughput     is %10f MB/sec\n", i, n, exec_t, throughput);
 
@@ -113,10 +121,10 @@ int main() {
 			for (k = 0; k < i; k++)
 				pthread_join(pthreads[k], NULL);
 			gettimeofday(&end_t, NULL);
-			exec_t = 1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0;
+			exec_t = (1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0)/10.0;
 			throughput = (n / (1024.0 * 1024.0)) / (exec_t / 1000.0);
 			printf("With %d threads, randomly    read %10d bytes, the latency is %10f ms and the throughput     is %10f MB/sec\n", i, n, exec_t, throughput);
 		}
 	}
-	remove(FILE_NAME);
+	//remove(FILE_NAME);
 }
