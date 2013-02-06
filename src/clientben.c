efe054bc944152be9c37b9ca3d79edb433e09298
Shukun Xie <shuk.xie@gmail.com>
2013-02-06 14:44:28 -0600

    Modified the network benchmark

--- src/clientben.c
+++ src/clientben.c
@@ -9,7 +9,7 @@
 #include <sys/time.h>
 #include <time.h>
 
-#define NUM_LOOPS 1000
+#define NUM_LOOPS 100
 
 struct thread_info {
 	int bufferSize;
@@ -25,11 +25,15 @@ void *udpClient(void *arg) {
 
 	int client_sock = socket(AF_INET, SOCK_DGRAM, 0);
 	int i = 0;
+	//printf("111\n");
 	for (i = 0; i < NUM_LOOPS; i++) {
 		sendto(client_sock, buffer, tinfo->bufferSize, 0, (struct sockaddr *)&(tinfo->server_addr), sizeof(struct sockaddr));
+		//printf("2 %d\n", i);
 		recvfrom(client_sock, buffer, tinfo->bufferSize, 0, (struct sockaddr *)&(tinfo->server_addr), &sin_size);
+		//printf("3 %d\n", i);
 	}
-	close(client_sock);
+	//printf("222\n");
+	//close(client_sock);
 }
 
 void *tcpClient(void *arg) {
@@ -37,15 +41,20 @@ void *tcpClient(void *arg) {
 	struct thread_info * tinfo = (struct thread_info *)arg;
 	char * buffer = (char *)malloc(sizeof(char)*(tinfo->bufferSize));
 	memset(buffer, 0, sizeof(char)*(tinfo->bufferSize));
-
+	//printf("%d\n", tinfo->bufferSize);
+	//printf("111\n");
 	int client_sock = socket(AF_INET, SOCK_STREAM, 0);
 	connect(client_sock, (struct sockaddr *)&(tinfo->server_addr), sizeof(struct sockaddr));
+	//printf("222\n");
 	int i = 0;
 	for (i = 0; i < NUM_LOOPS; i++) {
 		send(client_sock, buffer, tinfo->bufferSize, 0);
 		recv(client_sock, buffer, tinfo->bufferSize, 0);
+		//printf("%d\n", i);
 	}
-	close(client_sock);
+	//printf("333\n");
+	//free(buffer);
+	//close(client_sock);
 }
 
 int main(int argc, char *argv[]) {
@@ -69,27 +78,35 @@ int main(int argc, char *argv[]) {
 	pthread_t threads[numOfThreads];
 	
 	int i = 0;
+	//printf("aaa\n");
 	struct timeval start_t, end_t;
 	gettimeofday(&start_t, NULL);
-	if (strcmp(argv[1], "tcp") == 0) {
+	if (strcmp(argv[1], "TCP") == 0) {
+		//printf("bbb\n");
 		for (i = 0; i < numOfThreads; i++) {
 			pthread_create(&threads[i], NULL, tcpClient, tinfo);
 		}
+		//printf("ccc\n");
+		//sleep(36000);
 		for (i = 0; i < numOfThreads; i++) {
 			pthread_join(threads[i], NULL);
 		}
+		//printf("ddd\n");
 	} else {
+		//printf("aaa\n");
 		for (i = 0; i < numOfThreads; i++) {
 			pthread_create(&threads[i], NULL, udpClient, tinfo);
 		}
+		//printf("bbb\n");
 		for (i = 0; i < numOfThreads; i++) {
 			pthread_join(threads[i], NULL);
 		}
+		//printf("ccc\n");
 	}
 	gettimeofday(&end_t, NULL);
 	double exec_t = (1000.0*(end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec)/1000.0);
-	double throughput  = (numOfThreads * NUM_LOOPS * bufferSize / (1024 * 1024)) / (exec_t / 1000.0);
-	printf("With %d threads, the execution time is %10f ms and the throughput is %10f\n", numOfThreads, exec_t, throughput);
+	double throughput  = (numOfThreads * 2.0 * NUM_LOOPS * 8.0 * bufferSize / (1024.0 * 1024.0)) / (exec_t / 1000.0);
+	printf("With %d threads, the execution time is %10f ms and the throughput is %10f Mb/S\n", numOfThreads, exec_t, throughput);
    
 	return 0;
 }
--- src/serverben.c
+++ src/serverben.c
@@ -7,28 +7,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define NUM_LOOPS 100
+
 struct thread_info {
 	int sock;
 	int bufferSize;
 };
 
-struct udp_thread_info {
-	int sock;
-	int bufferSize;
-	struct sockaddr_in client_addr;
-};
-
 void *udp_send_data(void *t) {
-	struct udp_thread_info *tinfo = (struct udp_thread_info *)t;
+	struct thread_info *tinfo = (struct thread_info *)t;
+	struct sockaddr_in client_addr;
 	char * buff = (char *)malloc(sizeof(char)*tinfo->bufferSize);
 	memset(buff, 0, sizeof(char)*tinfo->bufferSize);
-	sendto(tinfo->sock, buff, tinfo->bufferSize, 0, (struct sockaddr *)&(tinfo->client_addr), sizeof(struct sockaddr));
+	int sin_size = sizeof(struct sockaddr_in);
+
+	int i = 0;
+	for (i = 0 ; i < NUM_LOOPS; i++) {
+		//printf("0 %d\n", i);
+		recvfrom(tinfo->sock, buff, tinfo->bufferSize, 0, (struct sockaddr *)&client_addr, &sin_size);
+		//printf("1 %d\n", i);
+		sendto(  tinfo->sock, buff, tinfo->bufferSize, 0, (struct sockaddr *)&client_addr, sizeof(struct sockaddr));
+		//printf("2 %d\n", i);
+	}
 }
 
 void udpServer(int bufferSize, int numOfThreads) {
 	int server_sock;
 	struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
 	char * buff = (char *)malloc(sizeof(char)*bufferSize);
 	memset(buff, 0, sizeof(char)*bufferSize);
 	memset(&server_addr, 0, sizeof(server_addr));
@@ -38,28 +43,31 @@ void udpServer(int bufferSize, int numOfThreads) {
 
 	server_sock = socket(AF_INET, SOCK_DGRAM, 0);
 	bind(server_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
-	int sin_size = sizeof(struct sockaddr_in);
 	int len = 0;
 	pthread_t threads[numOfThreads];
 	int i = 0;
-	while (1) {
-		recvfrom(server_sock, buff, bufferSize, 0, (struct sockaddr *)&client_addr, &sin_size);
-		struct udp_thread_info * t = (struct udp_thread_info *)malloc(sizeof(struct udp_thread_info));
-		t->sock = server_sock;
-		t->bufferSize = bufferSize;
-		t->client_addr = client_addr;
+	struct thread_info * t = (struct thread_info *)malloc(sizeof(struct thread_info));
+	t->sock = server_sock;
+	t->bufferSize = bufferSize;
+	for (i = 0; i < numOfThreads; i++) {
 		pthread_create(&threads[i], NULL, udp_send_data, t);
-		i++;
 	}
-	close(server_sock);
+	for (i = 0; i < numOfThreads; i++) {
+		pthread_join(threads[i], NULL);
+	}
+	//close(server_sock);
 }
 
 void *tcp_recv_data(void *t) {
 	struct thread_info *tinfo = (struct thread_info *)t;
 	char * buff = (char *)malloc(sizeof(char)*tinfo->bufferSize);
 	memset(buff, 0, sizeof(char)*tinfo->bufferSize);
-	recv(tinfo->sock, buff, tinfo->bufferSize, 0);
-	send(tinfo->sock, buff, tinfo->bufferSize, 0);
+	int i = 0;
+	for (i = 0; i < NUM_LOOPS; i++) {
+		recv(tinfo->sock, buff, tinfo->bufferSize, 0);
+		send(tinfo->sock, buff, tinfo->bufferSize, 0);
+	}
+	free(buff);
 }
 
 void tcpServer(int bufferSize, int numOfThreads) {
